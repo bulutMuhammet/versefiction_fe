849e1f2d4941cbcd3495adeedc0dc9a7391d0f52
Muhammet <muhammet.bulut@akinon.com>
2022-10-19 17:47:33 +0300

    new w update

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,14 @@
+var side_bar = document.getElementById("side_bar")
+
+document.getElementById("close_side_bar").addEventListener('click', closeSideBar)
+
 document.getElementById("accounts").addEventListener('click', openSideBar)
 function openSideBar(e){
     side_bar.classList.add("opened")
     side_bar.style.animation = "open_side_bar 0.5s forwards"
+}
+function closeSideBar(e){
+    side_bar.classList.remove("opened")
+    side_bar.style.animation = "close_side_bar 0.5s forwards"
+
 }
